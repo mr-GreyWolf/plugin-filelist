3854ce8721b85d19504e462ca411b2ddc6f210c3
mr-GreyWolf <mr.greywolf@list.ru>
2020-11-11 13:37:57 +0300

    Adding Russian translation

---
+++ lang/ru/lang.php
@@ -0,0 +1,17 @@
+<?php                                                                                                               
+/*                                                                                                                  
+ * additional language strings used by plugin                                                                       
+ *                                                                                                                  
+ * russian version                                                                                                  
+ *                                                                                                                  
+ */                                                                                                                 
+                                                                                                                    
+$lang['filename'] = 'Имя';                                                                                          
+$lang['filesize'] = 'Размер';                                                                                       
+$lang['lastmodified'] = 'Изменён';                                                                                  
+$lang['error_nomatch'] = 'Нет совпадений';                                                                          
+$lang['error_outsidejail'] = 'Доступ запрещён';                                                                     
+$lang['preview'] = 'Предпросмотр';                                                                                  
+$lang['filetype'] = 'Тип файла';                                                                                    
+$lang['createdby'] = 'Создатель';                                                                                   
+$lang['creatorunknown'] = 'Неизвестно';
